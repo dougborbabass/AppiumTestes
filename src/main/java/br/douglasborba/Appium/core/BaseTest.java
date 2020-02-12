367edbc747a42d8d3a1935d9e79452b4ae3bc471
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-02-12 09:48:15 -0300

    reset app apos testes

--- src/main/java/br/douglasborba/Appium/core/BaseTest.java
+++ src/main/java/br/douglasborba/Appium/core/BaseTest.java
@@ -1,12 +1,19 @@
 package br.douglasborba.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
-	
+
+	@AfterClass
+	public static void finalizaClasse() {
+		DriverFactory.killDriver();
+
+	}
+
 	@After
 	public void tearDown() {
-		DriverFactory.killDriver();
+		DriverFactory.getDriver().resetApp();
 	}
 
 }
